0e13ccdef8685fed5b1e7d5362c2ea9ac2db2e38
Manish123Sharma <manish2003sharma@gmail.com>
2022-04-14 21:29:41 +0530

    Update color_theme.dart

--- Music Project/music_app_class/lib/themes/color_theme.dart
+++ Music Project/music_app_class/lib/themes/color_theme.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 ThemeData getColorTheme() {
   return ThemeData(
-    scaffoldBackgroundColor: Colors.amberAccent,
-    backgroundColor: Colors.yellowAccent,
+    scaffoldBackgroundColor: Color.fromARGB(255, 238, 229, 198),
+    backgroundColor: Color.fromARGB(255, 243, 243, 203),
     appBarTheme: AppBarTheme(
       backgroundColor: Colors.greenAccent,
       foregroundColor: Colors.greenAccent)
