e98374f3f42db963f38d8ef5ba2775194a0ceeda
Manish123Sharma <manish2003sharma@gmail.com>
2022-04-14 21:35:02 +0530

    update

---
+++ firebase_class/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  runApp(Material());
+}
