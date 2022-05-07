04f0e7ae89c791c0911bd6d36ff618cd6abc56c7
Manish123Sharma <manish2003sharma@gmail.com>
2022-05-08 00:01:29 +0530

    update

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/screens/animatio.dart';
 import 'package:shop/screens/home2.dart';
 import 'package:shop/screens/intro_screens.dart';
 import 'package:shop/screens/rich_text.dart';
@@ -7,6 +8,7 @@ void main(List<String> args) {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     // home:home2()
-    home: RichTextExample(),
+    // home: RichTextExample(),
+    home: Animatedcontainer(),
   ));
 }
